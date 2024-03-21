cc31e9d25e7b0633d2e7facb05e7eca67987b417
Crystal Odi <crystalodi@gmail.com>
2024-03-21 01:53:40 -0500

    add theme and font styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 
 function App() {
-  document.body.setAttribute('data-theme', 'dark');
-  document.body.setAttribute('data-font-style', 'mono');
+  document.body.setAttribute('data-theme', 'light');
+  document.body.setAttribute('data-font-style', 'sans-serif');
   return (
     <div className="App">
       Aliquam
